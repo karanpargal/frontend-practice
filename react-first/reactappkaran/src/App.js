e219ce36775b0a5e6c185d07774ab6cf6a0a00f8
Karan <karanpargal007@gmail.com>
2022-08-30 14:21:43 +0530

    :sparkles: Practice project css

---
+++ react-first/reactappkaran/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/expenseItem";
+
+function app(){
+    return(
+        <div>
+            <h1>Hello from the other side.</h1>
+            <ExpenseItem />
+        </div>
+    )
+}
+
+export default app;
