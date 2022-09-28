78189c96e5552115104d0ff641f0dbeab7da402d
Karan <karanpargal007@gmail.com>
2022-09-28 21:20:45 +0530

    Add first component

--- react-first/reactappkaran/src/components/expenseItem.js
+++ react-first/reactappkaran/src/components/expenseItem.js
@@ -1,7 +1,21 @@
-function ExpenseItem() {
+import "./expenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: 'long' });
+  const day = props.date.getDate();
+  const year = props.date.getFullYear();
+
   return (
-    <div>
-      <h2>Expense Items!</h2>
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
